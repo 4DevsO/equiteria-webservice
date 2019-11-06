972cb98af4a0c00135b793d5e0d965e532b8c5da
Matheus Almeida <mat.almeida@live.com>
2019-11-06 11:03:46 -0300

    add eslint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const route = express.Router();
+const express = require('express')
+const route = express.Router()
 
-const PinRoute = require("./pin");
+const PinRoute = require('./pin')
 
-route.get("/", (req, res) => {
-  res.json({ success: true });
-});
+route.get('/', (req, res) => {
+  res.json({ success: true })
+})
 
-route.get("/pin", PinRoute.list);
-route.get("/pin/:pinId", PinRoute.index);
-route.post("/pin", PinRoute.store);
-route.put("/pin/:pinId", PinRoute.update);
-route.delete("/pin/:pinId", PinRoute.delete);
+route.get('/pin', PinRoute.list)
+route.get('/pin/:pinId', PinRoute.index)
+route.post('/pin', PinRoute.store)
+route.put('/pin/:pinId', PinRoute.update)
+route.delete('/pin/:pinId', PinRoute.delete)
 
-module.exports = route;
+module.exports = route
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const path = require("path");
-const express = require("express");
-const helmet = require("helmet");
-const compression = require("compression");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const firebase = require("firebase");
+const path = require('path')
+const express = require('express')
+const helmet = require('helmet')
+const compression = require('compression')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const firebase = require('firebase')
 
-const { catchAll, notFound } = require("./middlewares/error");
+const { catchAll, notFound } = require('./middlewares/error')
 
 // set env variables from .env
-dotenv.config();
+dotenv.config()
 
 // connect to mongodb
 mongoose.connect(process.env.MONGODB_URL, {
@@ -18,31 +18,31 @@ mongoose.connect(process.env.MONGODB_URL, {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-});
+})
 
 firebase.initializeApp(
-  require(path.resolve(__dirname, "config", "firebase_config.json"))
-);
+  require(path.resolve(__dirname, 'config', 'firebase_config.json'))
+)
 
-firebase.analytics();
+firebase.analytics()
 
-const app = express();
+const app = express()
 
 // express middlewares
-app.use(helmet());
-app.use(compression());
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(helmet())
+app.use(compression())
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // add routes
-app.use("/api/v1", require("./controllers"));
+app.use('/api/v1', require('./controllers'))
 
 // add errors middlewares
-app.use(notFound);
-app.use(catchAll);
+app.use(notFound)
+app.use(catchAll)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.info(`Service is running at http://localhost:${PORT}/api/v1`);
-});
+  console.info(`Service is running at http://localhost:${PORT}/api/v1`)
+})
